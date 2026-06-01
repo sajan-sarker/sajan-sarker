b256349ca0882e052d858e83a3d11908d6dd5a59
Sajan Sarker <sajansarker47@gmail.com>
2026-06-01 22:50:38 +0600

    portfolio contents

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -61,7 +61,7 @@ export default function HeroSection() {
           <div className="flex justify-center reveal">
             <div className="profile-wrap">
               <img
-                src="sajan-sarker/img/profile-illustration.png"
+                src="img/profile-illustration.png"
                 alt="Sajan Kumer Sarker profile"
                 className="profile-image"
               />
